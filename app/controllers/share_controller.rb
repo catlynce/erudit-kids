bf7860c4fa64e5ba020ed53b20d6e3b333aa8ace
Nuno Martins <nuno@alvesmartins.eu>
2020-04-20 06:38:11 +0100

    Add share route and controller

---
+++ app/controllers/share_controller.rb
@@ -0,0 +1,10 @@
+class ShareController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'share/index'
+  get 'share/new'
+  get 'share/create'
   devise_for :users
 
   get 'dashboard', to: 'dashboard#index'
+  get 'share/new', to: 'share#new'
+
   resources 'kids'
   resources 'resources'
 
