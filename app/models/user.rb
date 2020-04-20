c1fbb1d6fba5234978f53fd6a7038d010989e635
Nuno Martins <nuno@alvesmartins.eu>
2020-04-21 00:48:27 +0100

    First approach to associate account when registering a new account

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,27 @@ class User < ApplicationRecord
 
   before_validation :set_username
 
+  before_validation :verify_invited
+
   belongs_to :account
   has_many :kids, class_name: 'Kid', foreign_key: 'account_id'
-  # has_many :kids, through: :account, dependent: :destroy
-  # has_many :resources, dependent: :destroy
 
   private
 
   def set_username
     self.username = self.email if self.username.blank?
+  end
+
+  def verify_invited
+    account_id = Share.where(email: email).pluck(:account_id).first
+    unless account_id.nil?
+      self.account_id = account_id
+    else
+      part1 = Time.now.strftime('%Y%m%d')
+      part2 = ('a'..'z').to_a.shuffle[0,4].join.upcase
+      part3 = User.maximum(:id).next.to_s.rjust(6,'0')
+      account = Account.create uid: "#{part1}-#{part2}-#{part1}", plan: Plan.first
+      self.account_id = account
+    end
   end
 end
