07f8052027069badcc485431d60e4f1c91f56323
Nuno Martins <nuno@alvesmartins.eu>
2020-04-29 02:09:15 +0100

    Emit selected/unselected resources to be synced

--- app/javascript/components/ResourceItem.vue
+++ app/javascript/components/ResourceItem.vue
@@ -3,8 +3,12 @@
     <div
       :style="{ backgroundImage: `url(${resource.resource_image_url})` }"
       class="card resource relative w-100 h-full rounded border border-gray-300"
+      @click.prevent="toggleResource"
     >
-      <div class="absolute bottom-0 p-4 w-full bg-gray-700 rounded-b text-center">
+      <div
+        class="absolute bottom-0 p-4 w-full bg-gray-700 rounded-b text-center"
+        :class="{ ['bg-green-500 text-black']: isActive }"
+      >
         <a
           :href="resource.url"
           target="_blank"
@@ -21,13 +25,26 @@ export default {
   props: ["resource"],
   data() {
     return {
-      kidResources: null
+      toggle: false,
+      isActive: false
     };
   },
   mounted: () => {
-    console.log("testing");
+    // console.log("testing");
   },
-  methods: {}
+  methods: {
+    toggleResource() {
+      // console.log(this.resource);
+      this.toggle = !this.toggle;
+      if (this.toggle) {
+        this.isActive = true;
+        this.$emit("queue", this.resource.id);
+      } else {
+        this.isActive = false;
+        this.$emit("unqueue", this.resource.id);
+      }
+    }
+  }
 };
 </script>
 
--- app/javascript/components/ResourceList.vue
+++ app/javascript/components/ResourceList.vue
@@ -2,7 +2,14 @@
   <div>
     <h1 class="mb-4 font-sans font-bold text-2xl text-gray-800 tracking-wide">Resources</h1>
     <div class="grid grid-cols-4 gap-4">
-      <resource-item v-for="(item, index) in items" :index="index" :key="item.id" :resource="item"></resource-item>
+      <resource-item
+        v-for="(item, index) in items"
+        :index="index"
+        :key="item.id"
+        :resource="item"
+        @queue="queueResource"
+        @unqueue="unqueueResource"
+      ></resource-item>
     </div>
   </div>
 </template>
@@ -17,8 +24,23 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      resources: []
+    };
+  },
   components: {
     "resource-item": ResourceItem
+  },
+  methods: {
+    queueResource: function(resource) {
+      console.log("triggered queue");
+      this.resources.push(resource);
+    },
+    unqueueResource: function(resource) {
+      console.log("triggered unqueue");
+      this.resources.splice(this.resources.indexOf(resource), 1);
+    }
   }
 };
 </script>
